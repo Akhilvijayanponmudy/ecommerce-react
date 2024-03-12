c6bdb162d2c626725d58ff893586a813f562ef78
Akhil Vijayan <akhilvijayanponmudy@gmial.com>
2024-03-12 23:01:40 +0530

    updated product page

--- src/components/productsList/useProductsList.js
+++ src/components/productsList/useProductsList.js
@@ -2,36 +2,36 @@ import React from "react";
 import { Col } from 'react-bootstrap';
 import Style from './productsList.module.css'
 import { Link } from "react-router-dom";
+import baseURL from "../../api/apiConfig";
+const useProductList = ({ _id, productName,productActualPrice,productCurrentPrice, primaryImage}) => {
 
-const useLatestProducts = ({ id, imageUrl, altText, title, actualPrice, currentPrice, link }) => {
-
+    console.log(_id);
 
     // Function to calculate discount percentage
     const calculateDiscountPercentage = () => {
-        if (actualPrice === 0 || currentPrice === 0) {
+        if (productActualPrice === 0 || productCurrentPrice === 0) {
             return 0; // Return 0 if either price is 0 to avoid division by zero
         }
-        const discount = ((actualPrice - currentPrice) / actualPrice) * 100;
+        const discount = ((productActualPrice - productCurrentPrice) / productActualPrice) * 100;
         return discount.toFixed(2); // Round to 2 decimal places
     };
-
-
+    const imgUrl=baseURL+`uploads/`+primaryImage;
     return (
 
-        <Col key={id} xs={12} lg={3} >
+        <Col key={_id} xs={12} lg={3} >
 
-            <Link to={link} className={Style.linkWrap}>
+            <Link to={'/'} className={Style.linkWrap}>
 
                 <div className={Style.productWrap}>
                     <figure>
-                        <img src={imageUrl} alt={title} />
+                        <img src={imgUrl} alt={productName} />
                     </figure>
                     <div className={Style.ProductDetailsWrap}>
-                        <span className={Style.productTitle}>{title}</span>
+                        <span className={Style.productTitle}>{productName}</span>
                         <div className={Style.PriceWrap}>
-                            <span className={Style.CurrentPrice}> Rs. {currentPrice}</span>
-                            <span className={Style.oldPrice}> Rs. {actualPrice}</span>
-                            <span className={Style.discountPersantage}>Discount: {calculateDiscountPercentage()}%</span>
+                            <span className={Style.CurrentPrice}> Rs. {productCurrentPrice}</span>
+                            <span className={Style.oldPrice}> Rs. {productActualPrice}</span>
+                            <span className={Style.discountPersantage}>{calculateDiscountPercentage()} off</span>
                         </div>
                     </div>
 
@@ -43,4 +43,4 @@ const useLatestProducts = ({ id, imageUrl, altText, title, actualPrice, currentP
     )
 }
 
-export default useLatestProducts;
+export default useProductList;
