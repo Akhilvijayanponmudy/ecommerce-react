096d8a949c1f113e2d8171f1ffa267f833dcc6c6
Akhil <akhil.v@webandcrafts.com>
2024-05-22 18:33:40 +0530

    added styles

--- src/components/productCard/useProductCard.js
+++ src/components/productCard/useProductCard.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import styles from './useProductCard.module.css'
+import styles from './useProductCard.module.css';
 
-const useProductcard = ({ id, productName, productcat, Imgurl, altText, link }) => {
+const useProductcard = ({ id, productName, productcat, Imgurl, altText, link, productCurrentPrice, productActualPrice }) => {
     return (
         <Col key={id} xs={6} lg={3} className={styles.cardWrap}>
             <Link to={link} className={styles.productCardLinkWrap}>
@@ -15,6 +15,10 @@ const useProductcard = ({ id, productName, productcat, Imgurl, altText, link }) 
                         <div className={styles.productDetails}>
                             <p className={styles.brandName}>{productcat}</p>
                             <p className={styles.productName}>{productName}</p>
+                            <div className={styles.priceWrap}>
+                                <span className={styles.currentPrice}>Rs.  {productCurrentPrice}</span>
+                                <span className={styles.actualPrice}>Rs.  {productActualPrice}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ src/components/todaysDeals/offerCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OfferCard = () => {
+    return (
+        <h1>ffff</h1>
+    )
+}
+
+export default OfferCard
